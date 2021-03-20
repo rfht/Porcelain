601cb1c70098b26656b0f3feccd4eed28e00f3bd
thfrwn <11335318+rfht@users.noreply.github.com>
2021-03-19 21:04:49 -0600

    start moving subs to Porcelain.pm

--- script/Porcelain.pl
+++ script/Porcelain.pl
@@ -97,7 +97,7 @@ use List::Util qw(min max);
 require Net::SSLeay;
 use Pod::Usage;
 use Porcelain::RandomArt qw(randomart);
-#use Porcelain::Porcelain qw(clean_exit);
+use Porcelain::Porcelain qw(gen_client_cert gen_privkey);
 use Text::CharWidth qw(mbswidth);
 use Text::Wrap;
 
@@ -127,8 +127,6 @@ my $redirect_max = 5;	# TODO: allow setting this in the config
 
 # crypto stuff
 my $default_fp_algo = "SHA-256";
-my $rsa_exponent = 65537;	# 65537 is the 5th prime of Fermat and is the default for 'openssl genrsa'
-my $default_rsa_bits = 2048;
 
 my $porcelain_dir = $ENV{'HOME'} . "/.porcelain";
 my $idents_dir = $porcelain_dir . "/idents";
@@ -154,33 +152,6 @@ sub clean_exit {
 		print $_[0] . "\n";
 	}
 	exit;
-}
-
-sub gen_privkey {	# --> return private key
-	# see https://stackoverflow.com/questions/256405/programmatically-create-x509-certificate-using-openssl
-	my $pkey = Net::SSLeay::EVP_PKEY_new();
-	my $rsa = Net::SSLeay::RSA_generate_key($default_rsa_bits, $rsa_exponent) or die;	# returns value corresponding to openssl's RSA structur; other optional args: $perl_cb, $perl_cb_arg
-	Net::SSLeay::EVP_PKEY_assign_RSA($pkey, $rsa) or die;
-	#Net::SSLeay::RSA_free($rsa);	# TODO: not needed here?
-	#Net::SSLeay::EVP_PKEY_free($pkey);	# TODO: not needed here?
-	return $pkey;
-}
-
-sub gen_client_cert {	# days for cert validity, private key --> return cert
-	# see https://stackoverflow.com/questions/256405/programmatically-create-x509-certificate-using-openssl
-	my ($days_val, $pkey) = @_;
-	my $x509 = Net::SSLeay::X509_new();
-	die unless $x509;
-	Net::SSLeay::ASN1_INTEGER_set(Net::SSLeay::X509_get_serialNumber($x509), 1) or die;
-	Net::SSLeay::X509_gmtime_adj(Net::SSLeay::X509_get_notBefore($x509), 0) or die;
-	Net::SSLeay::X509_gmtime_adj(Net::SSLeay::X509_get_notAfter($x509), 60 * 60 * 24 * $days_val);
-	Net::SSLeay::X509_set_pubkey($x509, $pkey);
-	my $sname = Net::SSLeay::X509_get_subject_name($x509);
-	#Net::SSLeay::X509_set_subject_name($x509, $sname);
-	Net::SSLeay::X509_set_issuer_name($x509, $sname);	# subject name and issuer name are the same - self-signed cert
-	Net::SSLeay::X509_sign($x509, $pkey, Net::SSLeay::EVP_sha1());	# TODO: is SHA-1 a reasonable algorithm here?
-	#Net::SSLeay::X509_free($x509);	# TODO: needed here?
-	return $x509;
 }
 
 # TODO: merge store_privkey and store_cert into same function (very similar)
